304b3cfe59b8aa7429e94156aee513197d7668c0
ianhua <hya1994@qq.com>
2016-12-21 13:05:34 +0800

    twoSum

---
+++ twoSum.java
@@ -0,0 +1,24 @@
+/*
+ * Two Sum
+ * Use HashMap to store the (value, index) pair of each element so that 
+ * reduce the time of finding complement from O(n) to O(1). Because we
+ * do the check before put, so it is not a problem for duplicate elements.
+ */
+import java.util.*;
+public class twoSum {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int i, len, complement;
+		len = nums.length;
+		for (i = 0; i < len; i++) {
+			complement = target - nums[i];
+			if (map.containsKey(complement)) {
+				return new int[] {map.get(complement), i};
+			}
+			else {
+				map.put(nums[i], i);
+			}
+		}
+		return null;
+	}
+}
