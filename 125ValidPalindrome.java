82248d33fae8bbc1ff726b2bc1bf54d579e108d1
ianhua <hya1994@qq.com>
2017-01-19 14:02:09 +0800

    125

---
+++ 125ValidPalindrome.java
@@ -0,0 +1,18 @@
+/*
+ * 125. Valid Palindrome
+ * Move two pointers and only check letters and digits and ignore others
+ * O(n)
+ */
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0, end = s.length() - 1;
+        while (start < end) {
+            while (start < end && !Character.isLetterOrDigit(s.charAt(start))) start++;
+            while (start < end && !Character.isLetterOrDigit(s.charAt(end))) end--;
+            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
