1579d19ca61d802dc2bd2b2a0448fd8f65b87461
ianhua <hya1994@qq.com>
2017-01-28 12:33:52 +0800

    21

---
+++ 21MergeTwoSortedLists.java
@@ -0,0 +1,35 @@
+/**
+ * 21. Merge Two Sorted Lists
+ * LinkedList: use dummyHead and a cursor p
+ * Remember to update p = p.next
+ * O(n)
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null) p.next = l1;
+        if (l2 != null) p.next = l2;
+        return dummyHead.next;
+    }
+}
