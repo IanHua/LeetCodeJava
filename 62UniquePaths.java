066cef1d7f2ed98a016a805d80e3d04b3ba40606
ianhua <hya1994@qq.com>
2017-02-15 13:43:57 +0800

    62

---
+++ 62UniquePaths.java
@@ -0,0 +1,23 @@
+/**
+ * 62. Unique Paths
+ * Combination: C(m+n-2,m-1)
+ * Notice overflow, use long to store result
+ * O(n)
+ */
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) return 1;
+        long ans = 1;
+        int i, tmp;
+        if (m < n) {
+            tmp = m;
+            m = n;
+            n = tmp;
+        }
+        for (i = 1; i <= m - 1; i++) {
+            ans *= n - 1 + i;
+            ans /= i;
+        }
+        return (int)ans;
+    }
+}
