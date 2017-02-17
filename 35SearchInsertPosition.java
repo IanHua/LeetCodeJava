41453380e0512d0361bb40a79686f923e02cad6f
ianhua <hya1994@qq.com>
2017-02-17 10:20:10 +0800

    25

---
+++ 35SearchInsertPosition.java
@@ -0,0 +1,22 @@
+/**
+ * 35. Search Insert Position
+ * Binary Search
+ * while (base <= top)
+ * O(logn)
+ */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int base = 0, top = nums.length - 1, mid;
+        while (base <= top) {
+            mid = (base + top) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) {
+                base = mid + 1;
+            }
+            else {
+                top = mid - 1;
+            }
+        }
+        return base;
+    }
+}
