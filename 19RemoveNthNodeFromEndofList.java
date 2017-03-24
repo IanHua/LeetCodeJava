234941e05369581184cb991a2d237bb4b590cc3c
ianhua <hya1994@qq.com>
2017-03-24 14:24:21 +0800

    19

--- 19RemoveNthNodeFromEndofList.java
+++ 19RemoveNthNodeFromEndofList.java
@@ -1,5 +1,23 @@
 /**
  * 19. Remove Nth Node From End of List
- * 
+ * One pass by maintaining two pointers whose gap is n + 1
  * O(n)
  */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (n == 0) return head;
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode first = dummyHead;
+        ListNode second = dummyHead;
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummyHead.next;
+    }
+}
