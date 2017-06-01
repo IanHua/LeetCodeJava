98d2c04efe6e3083dcb73a20be9fb983f0ff5bc9
ianhua <hya1994@qq.com>
2017-06-01 19:54:45 +0800

    561

---
+++ 561ArrayPartition.cpp
@@ -0,0 +1,21 @@
+/**
+ * 561. Array Partition I
+ * The first C++ solution in LeetCode
+ * Just sort and pick every minimum
+ * O(nlogn)
+ */
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int len = nums.size();
+        if (len % 2 == 1) {
+            return 0;
+        }
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (int i = 0; i < len; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
