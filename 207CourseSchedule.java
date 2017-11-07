0eb5e31c774bc105f18cb682ac842c14aceaa456
ianhua <hya1994@qq.com>
2017-11-07 12:03:07 -0500

    207

--- 207CourseSchedule.java
+++ 207CourseSchedule.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // BFS
         List<Set<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
             graph.add(new HashSet<Integer>());
@@ -24,5 +25,30 @@ class Solution {
             }
         }
         return cnt == numCourses;
+        
+        /* DFS
+        List<Set<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new HashSet<Integer>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        boolean[] visited = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] || !dfs(graph, visited, i)) return false;
+        }
+        return true;*/
     }
+    /*
+    private boolean dfs(List<Set<Integer>> graph, boolean[] visited, int vertex) {
+        if (visited[vertex]) return false;
+        else visited[vertex] = true;
+        for (Integer adj : graph.get(vertex)) {
+            if (!dfs(graph, visited, adj)) return false;
+        }
+        visited[vertex] = false;
+        return true;
+    }
+    */
 }
