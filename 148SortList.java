f666c2b78dc57592176f3021bd2688885111f175
ianhua <hya1994@qq.com>
2017-11-09 16:13:16 -0500

    148

---
+++ 148SortList.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode head2 = slow;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(head2);
+        return merge(l1, l2);
+    }
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode cur = dummyNode;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return dummyNode.next;
+    }
+}
